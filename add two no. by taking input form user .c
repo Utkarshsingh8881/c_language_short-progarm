7b72395f6ff939a266332c11ba13bdf45ecb6faa
Utkarsh Singh <78108799+Utkarshsingh8881@users.noreply.github.com>
2022-05-26 01:02:09 +0530

    Create add two no. by taking input form user .c
    this program help us how to take input from user .

---
+++ add two no. by taking input form user .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num1 , num2;
+	printf("Enter num1 = ");
+	scanf("%d",&num1);
+	printf("Enter num2 = ");
+	scanf("%d",&num2);
+	
+	printf("Sum of two number is %d", num1+num2);
+	return 0;
+}
