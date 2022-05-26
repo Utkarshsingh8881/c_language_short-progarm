14fb66f7d0b584f35ebf302bac6389cca6400430
Utkarsh Singh <78108799+Utkarshsingh8881@users.noreply.github.com>
2022-05-26 16:52:40 +0530

    Create fibonacci series .c
    by using if-else and for loop.

---
+++ fibonacci series .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int num,  n1=0 , n2=1 ,n3;
+	printf("Enter the no. till you want fibonacci series : ");
+	scanf(" %d", &num);
+	printf("\n%d%d",n1,n2);
+	for(int i=2 ; i < num ; ++i){
+		n3=n1+n2;
+		printf("%d",n3);
+		n1=n2;
+		n2=n3;	
+	}
+	return 0;
+		
+	
+}
